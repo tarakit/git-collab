529d0188e80c55a6e62396bcd7cec0930cb303a5
Tara Kit <darakit069@gmail.com>
2023-11-01 11:35:16 +0700

    insert data to database

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
+
+    void printError(){
+        System.out.println("Testing Error");
+    }
     public static void main(String[] args) {
+        System.out.println("Error ...");
         System.out.println("Hello world!");
     }
 }
